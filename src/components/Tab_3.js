4ce7c296be6d7ef9e1202666d2c9762542c635ce
SumeetSinha <sumeet.kumar507@gmail.com>
2021-07-13 01:59:18 -0700

    implemented navigation between tabs

--- src/components/Tab_3.js
+++ src/components/Tab_3.js
@@ -26,7 +26,7 @@ class Tab_3 extends Component{
             <Form>
                 <Form.Group as={Row} controlId="ProjectName">
                   <Col sm={{ span: 2, offset: 0 }}>
-                    <Form.Label> &nbsp;&nbsp; Tab_2Project Name </Form.Label>
+                    <Form.Label> &nbsp;&nbsp; Tab_3Project Name </Form.Label>
                   </Col>
                   <Col sm={{ span: 3, offset: 0 }}>
                     <Form.Control type="text" placeholder="NewProject" defaultValue={this.props.inputValues.firstName} required onChange={this.props.handleChange}/>
@@ -35,7 +35,7 @@ class Tab_3 extends Component{
 
                 <Form.Group as={Row} controlId="Date">
                   <Col sm={{ span: 2, offset: 0 }}>
-                    <Form.Label> &nbsp;&nbsp; Tab_2Date </Form.Label>
+                    <Form.Label> &nbsp;&nbsp; Tab3Date </Form.Label>
                   </Col>
                   <Col sm={{ span: 3, offset: 0 }}>
                      <Form.Control type="Date" placeholder="Date" defaultValue={this.props.inputValues.lastName} required onChange={this.props.handleChange}/>
